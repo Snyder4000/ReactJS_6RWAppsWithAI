b3460a0176853ae4bfd3cf84371e66244fe99f0d
Mark Snyder <Snyder4000@yahoo.com>
2024-08-30 13:32:25 -0400

    Added Setup for the Translator App Project

---
+++ translator-app/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TranslatorStart from './Components/TranslatorStart'
+
+const App = () => {
+  return (
+    <div><TranslatorStart/></div>
+  )
+}
+
+export default App
