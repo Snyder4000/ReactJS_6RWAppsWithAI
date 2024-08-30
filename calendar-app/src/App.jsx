392548833f3fa35ab4e9a9a6934f5ebd36d969a9
Mark Snyder <Snyder4000@yahoo.com>
2024-08-30 10:51:42 -0400

    Added the Setup for the Calendar App Project

---
+++ calendar-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CalendarApp from './Components/CalendarApp'
+import './Components/CalendarApp.css'
+
+const App = () => {
+  return (
+    <div><CalendarApp/></div>
+  )
+}
+
+export default App
