418ad6c30630b31086293fca5cfcf2a1776b2d6c
Mark Snyder <Snyder4000@yahoo.com>
2024-09-18 14:55:04 -0400

    Added the state function and variable to switch between the starting of the ChatBotApp and the actually Chatting area for the ChatBotApp

--- ai-chat-bot/src/Components/ChatBotApp.jsx
+++ ai-chat-bot/src/Components/ChatBotApp.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './ChatBotApp.css'
 
-const ChatBotApp = () => {
+const ChatBotApp = ({ onGoBack }) => {
   return (
     <div className='chat-app'>
         <div className='chat-list'>
@@ -25,7 +25,7 @@ const ChatBotApp = () => {
         <div className="chat-window">
             <div className="chat-title">
                 <h3>Chat with AI</h3>
-                <i className="bx bx-arrow-back arrow"></i>
+                <i className="bx bx-arrow-back arrow" onClick= {onGoBack}></i>
             </div>
             <div className="chat">
                 <div className="prompt">Hi, how are you?
@@ -39,7 +39,7 @@ const ChatBotApp = () => {
             <form className='msg-form'>
                 <i className="fa-solid fa-face-smile emoji"></i>
                 <input type='text' className='msg-input' placeholder='Type a message...'/>
-                <i className="fa-solid fa-paper-plane"></i>
+                <i className="fa-solid fa-paper-plane" onClick= {onGoBack}></i>
             </form>
         </div>
     </div>
