6fb15b7013048d428ddeb26426c6d35f8467022d
Mark Snyder <Snyder4000@yahoo.com>
2024-10-29 14:09:06 -0400

    Finished the Quotes generator app, one issue with the api the quotes comes from, it needs to be approved by the web browser before it works every time

--- qoutes-app/src/Components/QuotesApp.jsx
+++ qoutes-app/src/Components/QuotesApp.jsx
@@ -5,6 +5,8 @@ const QuotesApp = () => {
     text: "Ask not what your country can do for you; ask what you can do for your country",
     author: "John Kennedy",
   })
+  const [favorites, setFavorites] = useState([])
+  const [showFavorites, setShowFavorites] = useState(false)
 
   const fetchNewQuote = async () =>{
     const url = 'https://api.quotable.io/random'
@@ -16,11 +18,23 @@ const QuotesApp = () => {
     })
   }
 
+  const toggleFavorites = () =>{
+    setShowFavorites(!showFavorites)
+  }
+
+  const addToFavorites = () =>{
+    const isAlreadyInFavorites = favorites.some((fav) => fav.text === quote.text && fav.author === quote.author)
+
+    if(!isAlreadyInFavorites){
+      setFavorites([...favorites, quote])
+    }
+  }
+
   return (
     <div className='container'>
       <div className="quotes-app">
         <h1 className='app-heading'>Quote.</h1>
-        <i className="bx bxs-heart fav-icon"></i>
+        <i className="bx bxs-heart fav-icon" onClick={toggleFavorites}></i>
         <div className="quote">
           <i className="bx bxs-quote-alt-left left-quote"></i>
           <p className="quote-text">{quote.text}</p>
@@ -34,9 +48,32 @@ const QuotesApp = () => {
           <div className="circle-4"></div>
         </div>
         <div className="buttons">
-          <button className='btn btn-new' onClick={fetchNewQuote}>New Quote</button>
-          <button className='btn btn-fav'>Add to Favorites</button>
+          <button className='btn btn-new' onClick={fetchNewQuote}>
+            New Quote
+          </button>
+          <button className='btn btn-fav' onClick={addToFavorites}>Add to Favorites</button>
         </div>
+        {showFavorites && (
+          <div className="favorites">
+            <button className="btn-close" onClick={toggleFavorites}>
+              <i className="bx bx-x"></i>
+            </button>
+            {favorites.map((favQuote, index) => (
+              <div className="fav-quote" key={index}>
+                <div className="fav-quote-delete">
+                  <i className="bx bx-x-circle" onClick={() => {
+                    const updatedFavorites = favorites.filter((item, i) => i !== index)
+                    setFavorites(updatedFavorites)
+                  }}></i>
+                </div>
+                <div className="fav-quote-content">
+                  <div className="fav-quote-text">{favQuote.text}</div>
+                  <div className="fav-quote-author">{favQuote.author}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   )
